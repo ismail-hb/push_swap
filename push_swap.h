a087f91f75716b4025c94ff32f3345b05bc3aba9
Ismail Houche <ishouche@z1r9p1.42lyon.fr>
2024-04-06 01:26:51 +0200

    lastreview

---
+++ push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ishouche <ishouche@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 10:09:05 by ishouche          #+#    #+#             */
+/*   Updated: 2024/01/29 10:14:16 by ishouche         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+# endif
