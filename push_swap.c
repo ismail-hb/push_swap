1c324eebe9535a7b0554c37d0b23d47d862256dc
Ismail Houche <ishouche@z3r12p1.42lyon.fr>
2024-04-07 03:16:32 +0200

    main done

--- push_swap.c
+++ push_swap.c
@@ -6,14 +6,33 @@
 /*   By: ishouche <ishouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 10:14:43 by ishouche          #+#    #+#             */
-/*   Updated: 2024/04/06 00:12:30 by ishouche         ###   ########.fr       */
+/*   Updated: 2024/04/07 03:11:09 by ishouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include "libft/libft.h"
 
 int	main(int argc, char **argv)
 {
+	t_list	*a;
+	t_list	*b;
 
+	a = NULL;
+	b = NULL;
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
+		return(ft_printf("Error"), 1);
+	else if (argc == 2)
+		argv = split(argv[1], ' ');
+	init_a(&a, argv + 1);
+	if (!pile_is_sorted(a))
+	{
+		if (pile_len(a) == 2)
+			sa(&a);
+		else if (pile_len(a) <= 5)
+			small_sort(&a, &b);
+		else
+			big_sort(&a, &b);
+	}
+	free_pile(&a);
+	return(0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ishouche <ishouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 10:09:05 by ishouche          #+#    #+#             */
-/*   Updated: 2024/01/29 10:14:16 by ishouche         ###   ########.fr       */
+/*   Updated: 2024/04/07 02:42:50 by ishouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,15 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdio.h>
+# include "libft/libft.h"
+# include <limits.h>
 
 typedef struct s_list
 {
-	void			*content;
+	int				nbr;
 	struct s_list	*next;
+	struct s_list	*prev;
 }	t_list;
 
 # endif
